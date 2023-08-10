7c3886daa77c0b83b97c13b7b98bec6ed3ae4b54
alisadeghi <alisadghi228@gmail.com>
2023-08-10 22:18:41 +0330

    create login controller and validate data in login

--- app/http/controllers/auth.controller.js
+++ app/http/controllers/auth.controller.js
@@ -1,31 +1,46 @@
 const { UserModel } = require("../../models/User");
 const { HashString } = require("../../modules/functions");
-
+const bcrypt = require("bcrypt");
 class AuthController {
   async register(req, res, next) {
     try {
-        const { username, password, email, mobile } = req.body;
-        const hash_password = HashString(password);
-        const user = await UserModel.create({
-          username,
-          email,
-          mobile,
-          password: hash_password,
-        }).catch((err) => {
-          if (err?.code === 11000) {
-            throw {
-              status: 400,
-              message: "نام کاربری قبلا در سیستم اضافه شده است",
-            };
-          }
-        });
-        user.password = undefined;
-        return res.json({ user });
+      const { username, password, email, mobile } = req.body;
+      const hash_password = HashString(password);
+      const user = await UserModel.create({
+        username,
+        email,
+        mobile,
+        password: hash_password,
+      }).catch((err) => {
+        if (err?.code === 11000) {
+          throw {
+            status: 400,
+            message: "نام کاربری قبلا در سیستم اضافه شده است",
+          };
+        }
+      });
+      user.password = undefined;
+      return res.json({ user });
     } catch (error) {
-        next(error);
+      next(error);
     }
   }
-  login() {}
+  async login(req, res, next) {
+    try {
+      const { username, password } = req.body;
+      const user = await UserModel.findOne({ username });
+      if (!user) {
+        res.status(401).json({ message: "نام کاربری یا رمز عبور اشتباه است." });
+      }
+      const compareResult = bcrypt.compareSync(password, user.password);
+      if (!compareResult) {
+        res.status(401).json({ message: "نام کاربری یا رمز عبور اشتباه است." });
+      }
+      return res.status(200).json({ message: "شما با موفقیت احراز شدید." });
+    } catch (error) {
+      next(error);
+    }
+  }
   resetPassword() {}
 }
 
--- app/http/validations/auth.js
+++ app/http/validations/auth.js
@@ -1,55 +1,71 @@
-const {body} = require('express-validator');
-const { UserModel } = require('../../models/User');
+const { body } = require("express-validator");
+const { UserModel } = require("../../models/User");
 
-function register(){
-    return [
-      body("username")
-        .isLength({ min: 4, max: 25 })
-        .custom(async (value, ctx) => {
-          if (value) {
-            const usernameRegex = /^[a-z]+[a-z0-9\_\.]{2,}/gi;
-            if (usernameRegex.test(value)) {
-              const user = await UserModel.findOne({ username: value });
-              if (user) {
-                throw "نام کاربری تکراری میباشد.";
-              }
-              return true;
+function register() {
+  return [
+    body("username")
+      .isLength({ min: 4, max: 25 })
+      .custom(async (value, ctx) => {
+        if (value) {
+          const usernameRegex = /^[a-z]+[a-z0-9\_\.]{2,}/gi;
+          if (usernameRegex.test(value)) {
+            const user = await UserModel.findOne({ username: value });
+            if (user) {
+              throw "نام کاربری تکراری میباشد.";
             }
-            throw "نام کاربری صحیح نمیباشد.";
+            return true;
           }
-          throw "نام کاربری نمیتواند خالی باشد.";
-        }),
-      body("email")
-        .isEmail()
-        .withMessage("ایمیل وارد شده صحیح نمیباشد.")
-        .custom(async (email) => {
-          const user = await UserModel.findOne({ email });
-          if (user) {
-            throw "ایمیل وارد شده تکراری میباشد.";
-          }
-        }),
-      body("mobile")
-        .isMobilePhone("fa-IR")
-        .withMessage("شماره موبایل وارد شده صحیح نمیباشد.")
-        .custom(async (mobile) => {
-          const user = await UserModel.findOne({ mobile });
-          if (user) {
-            throw "شماره موبایل وارد شده تکراری میباشد.";
-          }
-        }),
-      body("password")
-        .isLength({ min: 6, max: 16 })
-        .withMessage("رمز عبور حداقل باید 6 و حداکثر 16 نویسه باشد.")
-        .custom((value, ctx) => {
-          if (!value) throw "رمز عبور نمیتواند خالی باشد.";
-          if (value !== ctx?.req?.body?.confirm_password) {
-            throw "رمز عبور با تکرار آن یکسان نمیباشد.";
-          }
+          throw "نام کاربری صحیح نمیباشد.";
+        }
+        throw "نام کاربری نمیتواند خالی باشد.";
+      }),
+    body("email")
+      .isEmail()
+      .withMessage("ایمیل وارد شده صحیح نمیباشد.")
+      .custom(async (email) => {
+        const user = await UserModel.findOne({ email });
+        if (user) {
+          throw "ایمیل وارد شده تکراری میباشد.";
+        }
+      }),
+    body("mobile")
+      .isMobilePhone("fa-IR")
+      .withMessage("شماره موبایل وارد شده صحیح نمیباشد.")
+      .custom(async (mobile) => {
+        const user = await UserModel.findOne({ mobile });
+        if (user) {
+          throw "شماره موبایل وارد شده تکراری میباشد.";
+        }
+      }),
+    body("password")
+      .isLength({ min: 6, max: 16 })
+      .withMessage("رمز عبور حداقل باید 6 و حداکثر 16 نویسه باشد.")
+      .custom((value, ctx) => {
+        if (!value) throw "رمز عبور نمیتواند خالی باشد.";
+        if (value !== ctx?.req?.body?.confirm_password) {
+          throw "رمز عبور با تکرار آن یکسان نمیباشد.";
+        }
+        return true;
+      }),
+  ];
+}
+
+function login() {
+  return [
+    body("username")
+      .notEmpty()
+      .withMessage("نام کاربری نمیتواند خالی باشد")
+      .custom((username) => {
+        const usernameRegex = /^[a-z]+[a-z0-9\_\.]{2,}/gi;
+        if (usernameRegex.test(username)) {
           return true;
-        }),
-    ];
+        }
+        throw "نام کاربری صحیح نمیباشد.";
+      })
+  ];
 }
 
 module.exports = {
-    registerValidator : register
-}
+  registerValidator: register,
+  loginValidator: login,
+};
