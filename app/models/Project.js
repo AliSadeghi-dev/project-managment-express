11632eee19d6c430482d16ca3c501bdee8ba6b15
alisadeghi <alisadghi228@gmail.com>
2023-08-10 15:57:54 +0330

    create models

---
+++ app/models/Project.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+      default: "/defaults/project-management-header.png",
+    },
+    private: {
+      type: Boolean,
+      default: true,
+    },
+    team: {
+      type: mongoose.Types.ObjectId,
+    },
+    owner: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProjectModel = mongoose.model("Project", ProjectSchema);
+
+module.exports = {
+  ProjectModel,
+};
---
+++ app/models/Team.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const TeamSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    users: {
+      type: [mongoose.Types.ObjectId],
+      default: "",
+    },
+    owner: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const TeamModel = mongoose.model("Team", TeamSchema);
+
+module.exports = {
+  TeamModel,
+};
