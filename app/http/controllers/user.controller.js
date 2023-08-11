6250101e74f05e82cbd81efe94d6c2ec46a22510
alisadeghi <alisadghi228@gmail.com>
2023-08-11 10:43:34 +0330

    finish edit profile service

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const { UserModel } = require("../../models/User");
+
 class UserController {
   getProfile(req, res, next) {
     try {
@@ -11,7 +13,30 @@ class UserController {
       next(error);
     }
   }
-  editProfile(req, res, next) {}
+  async editProfile(req, res, next) {
+    try {
+        const data = req.body;
+        const userId = req.user._id;
+        const filed = ["first_name", "last_name", "skills"];
+        const badValues = ['',' ',undefined,null,NaN,0,-1]
+        Object.entries(data).forEach(([key, value]) =>{
+            if (!filed.includes(key)) delete data[key];
+            if (badValues.includes(value)) delete data[key];
+        })
+        const user = await UserModel.updateOne({_id:userId},{$set:data});
+        if(user.modifiedCount > 0){
+            return res.status(200).json({
+                status:200,
+                success:true,
+                message:'بروزرسانی پروفایل با موفقیت انجام شد.'
+            })
+        }
+        throw {status:400,message:'بروز رسانی انجام نشد.'}
+    } catch (error) {
+        next(error);
+    }
+
+  }
   addSkills(req, res, next) {}
   editSkills(req, res, next) {}
   acceptInviteInTeam(req, res, next) {}
--- app/router/user.js
+++ app/router/user.js
@@ -4,6 +4,7 @@ const { checkLogin } = require("../modules/checkLogin");
 const router = require("express").Router();
 
 router.get("/profile",checkLogin, UserController.getProfile);
+router.post("/profile", checkLogin, UserController.editProfile);
 
 module.exports = {
   userRoutes: router,
